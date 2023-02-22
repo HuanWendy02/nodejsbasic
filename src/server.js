854136594bdf411a18eab8f72ad6b22dd0c849d4
Huan Wendy <nguyenhuanrv@gmail.com>
2023-02-22 23:06:44 +0700

    video7

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
+require('dotenv').config();
 const app = express();
-const port = 8080
+const port = process.env.port || 8080;
 configViewEngine(app);
 app.get('/', (req, res) => {
     res.render('test/index.ejs');
