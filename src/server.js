6f4dc31c0219e753dbec06d44a15928113b8924b
Huan Wendy <nguyenhuanrv@gmail.com>
2023-02-25 10:02:16 +0700

    video8

--- src/server.js
+++ src/server.js
@@ -1,17 +1,16 @@
 
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
+import initWebRoute from './route/web';
 require('dotenv').config();
 const app = express();
 const port = process.env.port || 8080;
-configViewEngine(app);
-app.get('/', (req, res) => {
-    res.render('test/index.ejs');
 
-})
-app.get('/about', (req, res) => {
-    res.send(`I'm Irene`)
-})
+// setup view engine
+configViewEngine(app);
+//init web route
+initWebRoute(app);
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
